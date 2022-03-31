876f3839efdc8dea3951b04fe5a7d9ceb3a2f902
FddDaiErBai <99399205+FddDaiErBai@users.noreply.github.com>
2022-03-31 10:37:13 +0800

    添加编码标识
    Complete 整数类型

--- Chap1/输出函数print.py
+++ Chap1/输出函数print.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
 """
 author: FddDaiErBai
 date: 2022.03.30
--- Chap2/变量的定义和使用.py
+++ Chap2/变量的定义和使用.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
 """
 author: FddDaiErBai
 date: 2022.03.30
 """
+
 name = '玛利亚'
 print(name)
 print('标识', id(name))
