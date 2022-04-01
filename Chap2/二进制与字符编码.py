70e290dd854ed904dff6ed1c08026e28edd4693e
FddDaiErBai <99399205+FddDaiErBai@users.noreply.github.com>
2022-04-01 13:58:51 +0800

    格式修改
    完成浮点类型

--- Chap2/二进制与字符编码.py
+++ Chap2/二进制与字符编码.py
@@ -5,8 +5,7 @@
 author: FddDaiErBai
 date: 2022.03.30
 """
+
 print(chr(0b100111001011000))
 print(ord('乘'))
 print(int(ord('乘')))
-
-
---
+++ Chap2/浮点类型.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+"""
+author: FddDaiErBai
+date: 2022.04.01
+"""
+
+a = 3.14159
+print(a, type(a))
+
+n_1 = 1.1
+n_2 = 2.2
+print(n_1+n_2)
