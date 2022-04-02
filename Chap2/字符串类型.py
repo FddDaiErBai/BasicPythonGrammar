7c3d9027ac5f4a45c2677ba1716e28e78359b3c6
FddDaiErBai <99399205+FddDaiErBai@users.noreply.github.com>
2022-04-02 08:24:50 +0800

    Create 字符串类型.py
    完成 字符串类型

---
+++ Chap2/字符串类型.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+"""
+author: FddDaiErBai
+date: 2022.04.02
+"""
+
+str1='人生苦短,我用python'
+str2="人生苦短,我用python"
+str3="""人生苦短,
+        我用python
+     
+     """
+str4='''人生苦短,
+        我用python
+     
+    '''
+
+print(str1,type(str1))
+print(str2,type(str2))
+print(str3,type(str3))
+print(str4,type(str4))
