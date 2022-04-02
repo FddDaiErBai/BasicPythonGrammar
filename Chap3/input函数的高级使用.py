05d5c81550b1abcc44e9abadf00b86821b6c5719
FddDaiErBai <99399205+FddDaiErBai@users.noreply.github.com>
2022-04-02 10:32:38 +0800

    Create input函数的高级使用.py
    完成 input函数的高级使用

---
+++ Chap3/input函数的高级使用.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+"""
+author: FddDaiErBai
+date: 2022.04.02
+"""
+
+# 从键盘录入两个整数，计算两个整数和
+a = input('请输入一个加数：\n')
+b = input('请输入另一个加数：\n')
+
+print(a+b)
+print(type(a), type(b))
+
+a = int(a)
+b = int(b)
+
+# 或者 a=int(input('请输入一个加数'))
+
+print(a+b)
