d5fb1c9228e8ac2e42807bc69c1f2c4523a1c07a
FddDaiErBai <99399205+FddDaiErBai@users.noreply.github.com>
2022-04-02 08:12:01 +0800

    Create 布尔类型.py
    完成 布尔类型

---
+++ Chap2/布尔类型.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+"""
+author: FddDaiErBai
+date: 2022.04.02
+"""
+
+f1 = False
+f2 = True
+
+print(f1, type(f1))
+print(f2, type(f2))
+
+print(f1+1)  # 0+1的结果
+print(f2+1)  # 1+1的结果
