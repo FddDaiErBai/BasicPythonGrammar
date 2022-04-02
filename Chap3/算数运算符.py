4e0c08b7b8d6a8798f74bd5d842638f70e73aac0
FddDaiErBai <99399205+FddDaiErBai@users.noreply.github.com>
2022-04-02 15:01:24 +0800

    Create 算数运算符.py
    完成 算数运算符

---
+++ Chap3/算数运算符.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+"""
+author: FddDaiErBai
+date: 2022.04.02
+"""
+
+print(1+1)  # 加法运算
+print(1-1)  # 减法运算
+print(2*4)  # 乘法运算
+print(1/2)  # 除法运算
+print(11/2)
+print(11//2)  # 整除运算
+print(11 % 2)  # 取余运算
+print(2**3)  # 幂运算
+
+print(9//4)  # 2
+print(-9//-4)  # 2
+print(9//-4)  # -3
+print(-9//4)  # -3
+# 一正一负，向下取整
+
+print(9 % -4)  # -3 # 9-(-4)*(-3)=-3
+print(-9 % 4)  # 3 #  -9-4*(-3)=3
+# 余数=被除数-除数*商
