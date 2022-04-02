46fee3c9a7cdd4eb556110c37077fdbfd9d18c1f
FddDaiErBai <99399205+FddDaiErBai@users.noreply.github.com>
2022-04-02 17:10:02 +0800

    Create 布尔运算符.py
    完成 布尔运算符

---
+++ Chap3/布尔运算符.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+"""
+author: FddDaiErBai
+date: 2022.04.02
+"""
+
+# 布尔运算符
+
+# and
+a, b = 1, 2
+print(a == 1 and b == 2)  # True
+print(a == 1 and b < 2)  # False
+print(a != 1 and b == 2)  # False
+print(a != 1 and b != 2)  # False
+
+# or
+print(a == 1 or b == 2)  # True
+print(a == 1 or b < 2)  # True
+print(a != 1 or b == 2)  # True
+print(a != 1 or b != 2)  # False
+
+# not
+f = True
+f2 = False
+print(not f)
+print(not f2)
+
+# in
+s = 'Hello,world'
+print('w' in s)
+print('k' in s)
+print('w' not in s)
+print('k' not in s)
