6ab00ddb8e7b5095c832ab197dfb747b4439301d
FddDaiErBai <99399205+FddDaiErBai@users.noreply.github.com>
2022-04-04 00:46:21 +0800

    修改文件名
    完成 单分支结构

---
+++ Chap4/单分支结构.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+
+"""
+author: FddDaiErBai
+date: 2022.04.04
+"""
+
+money=1000 # 余额
+s=int(input('请输入取款金额:\n')) #  取款金额
+# 判断余额是否充足
+if money>=s:
+    money-=s
+    print('取款成功，余额为:',money)
