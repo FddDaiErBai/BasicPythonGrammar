6178ee2c0341a97acb3d16c2599ac6dc8691d85f
FddDaiErBai <99399205+FddDaiErBai@users.noreply.github.com>
2022-04-04 00:51:39 +0800

    对齐
    完成 双分支结构

--- Chap4/单分支结构.py
+++ Chap4/单分支结构.py
@@ -5,9 +5,9 @@ author: FddDaiErBai
 date: 2022.04.04
 """
 
-money=1000 # 余额
-s=int(input('请输入取款金额:\n')) #  取款金额
+money = 1000  # 余额
+s = int(input('请输入取款金额:\n'))  # 取款金额
 # 判断余额是否充足
-if money>=s:
-    money-=s
-    print('取款成功，余额为:',money)
+if money >= s:
+    money -= s
+    print('取款成功，余额为:', money)
