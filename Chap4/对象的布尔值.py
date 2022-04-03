10f54b385e913dc0591b649b75d106a302749e97
FddDaiErBai <99399205+FddDaiErBai@users.noreply.github.com>
2022-04-04 00:31:38 +0800

    Create 对象的布尔值.py
    完成 对象的布尔值

---
+++ Chap4/对象的布尔值.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+
+"""
+author: FddDaiErBai
+date: 2022.04.03
+"""
+
+# 测试对象的布尔值
+print(bool(False))  # False
+print(bool(0))  # False
+print(bool(0.0))  # False
+print(bool(None))  # False
+print(bool(''))  # False
+print(bool(""))  # False
+print(bool([]))  # 空列表
+print(bool(list()))  # 空列表
+print(bool(()))  # 空元组
+print(bool(tuple()))  # 空元组
+print(bool({}))  # 空字典
+print(bool(dict()))  # 空字典
+print(bool(set()))  # 空集合
+
+print(bool(18))
+print(bool(True))
+print(bool('Hello,world'))
