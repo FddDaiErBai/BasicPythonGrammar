cbe18d5a83c01d243ac755de909fedd0b7e07a4f
FddDaiErBai <99399205+FddDaiErBai@users.noreply.github.com>
2022-04-04 23:31:30 +0800

    Create 嵌套循环.py
    完成 嵌套循环

---
+++ Chap5/嵌套循环.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+
+"""
+author: FddDaiErBai
+date: 2022.04.04
+"""
+
+# 输出三行四列的矩形
+
+for i in range(1, 4):
+    for j in range(1, 5):
+        print('*', end='\t')
+    print()
