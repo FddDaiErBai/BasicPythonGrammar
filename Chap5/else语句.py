9fa41f85ca3d0b60e9be34ea1551374d0087519f
FddDaiErBai <99399205+FddDaiErBai@users.noreply.github.com>
2022-04-04 23:25:32 +0800

    修改while
    完成 else语句

--- Chap5/else语句.py
+++ Chap5/else语句.py
@@ -5,8 +5,6 @@ author: FddDaiErBai
 date: 2022.04.04
 """
 
-# 从键盘录入密码,最多录入3次,如果正确就结束循环
-
 for item in range(3):
     pwd = input('请输入密码:')
     if pwd == '8888':
@@ -14,6 +12,8 @@ for item in range(3):
         break
     else:
         print('密码错误')
+else:
+    print('对不起,三次密码输入均输入错误')
 
 a = 0
 while a < 3:
@@ -24,3 +24,5 @@ while a < 3:
     else:
         print('密码错误')
     a += 1
+else:
+    print('对不起,三次密码输入均输入错误')
