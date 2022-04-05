f084063e0a26ef1469ed0e66e6c15af1e9257d5f
FddDaiErBai <99399205+FddDaiErBai@users.noreply.github.com>
2022-04-06 00:44:35 +0800

    Update 获取字典视图.py
    完成 获取字典视图

--- Chap7/获取字典视图.py
+++ Chap7/获取字典视图.py
@@ -7,19 +7,19 @@ date: 2022.04.06
 
 scores = {'张三': 100, '李四': 8, '王五': 45}
 # 获取所有的key
-keys=scores.keys()
+keys = scores.keys()
 print(keys)
 print(type(keys))
-print(list(keys)) # 将所有key组成的视图转成列表
+print(list(keys))  # 将所有key组成的视图转成列表
 
 # 获取所有的value
-values=scores.values()
+values = scores.values()
 print(values)
 print(type(values))
 print(list(values))
 
-# 获取所有的key-value对
-items=scores.items()
+# 获取所有的key-value对 ()是元组类型
+items = scores.items()
 print(items)
 print(type(items))
 print(list(items))
