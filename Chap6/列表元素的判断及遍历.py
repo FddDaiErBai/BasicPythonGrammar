27c314d261d8d5ed7f8430b6f7aa666c8f6160e0
FddDaiErBai <99399205+FddDaiErBai@users.noreply.github.com>
2022-04-05 23:17:12 +0800

    Create 列表元素的判断及遍历.py
    完成 列表元素的判断及遍历

---
+++ Chap6/列表元素的判断及遍历.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+
+"""
+author: FddDaiErBai
+date: 2022.04.05
+"""
+
+lst = ['Hello', 29, 20, 'world']
+print(10 in lst)
+print(20 in lst)
+
+for item in lst:
+    print(item)
