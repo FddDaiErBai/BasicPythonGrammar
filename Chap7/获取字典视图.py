e88c3c937d2521cedf7b70a5954e52bec238b0b4
FddDaiErBai <99399205+FddDaiErBai@users.noreply.github.com>
2022-04-06 00:43:33 +0800

    Create 获取字典视图.py
    完成 获取字典视图

---
+++ Chap7/获取字典视图.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+
+"""
+author: FddDaiErBai
+date: 2022.04.06
+"""
+
+scores = {'张三': 100, '李四': 8, '王五': 45}
+# 获取所有的key
+keys=scores.keys()
+print(keys)
+print(type(keys))
+print(list(keys)) # 将所有key组成的视图转成列表
+
+# 获取所有的value
+values=scores.values()
+print(values)
+print(type(values))
+print(list(values))
+
+# 获取所有的key-value对
+items=scores.items()
+print(items)
+print(type(items))
+print(list(items))
