4e09b090a3e6916b98142becb54f49294d6ed53d
FddDaiErBai <99399205+FddDaiErBai@users.noreply.github.com>
2022-04-06 00:06:44 +0800

    Create 列表元素的生成式.py
    完成 列表元素的生成式

---
+++ Chap6/列表元素的生成式.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+
+"""
+author: FddDaiErBai
+date: 2022.04.06
+"""
+
+# 使用列表生生成式,列表元素需要有一定规则
+lst = [i*i for i in range(1, 10)]
+print(lst)
+
+lst2 = [i*2 for i in range(1, 10)]
+print(lst2)
