ba94b7b3f9923b01139cf185e8c167f5758436bf
FddDaiErBai <99399205+FddDaiErBai@users.noreply.github.com>
2022-04-09 00:14:19 +0800

    Create Python中常见的异常类型.py
    完成 Python中常见的异常类型

---
+++ Chap11/Python中常见的异常类型.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+
+"""
+author: FddDaiErBai
+date: 2022.04.09
+"""
+
+# ZreoDivisionError 除0
+# IndexError 序列中没有次索引
+# KeyError 映射中没有这个键
+# NameError 未声明/初始化对象(没有属性)
+# SyntaxError Python语法错误
+# ValueError 传入无效的参数
