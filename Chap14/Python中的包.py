99ac56b3b32ce591949a291643195b9cb24a5706
FddDaiErBai <99399205+FddDaiErBai@users.noreply.github.com>
2022-04-09 17:07:29 +0800

    Create Python中的包.py
    完成 Python中的包

---
+++ Chap14/Python中的包.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+
+"""
+author: FddDaiErBai
+date: 2022.04.09
+"""
+
+# 导入package1包
+import package1.mouduleA as ma  # ma是模块的别名
+
+# print(package1.mouduleA.a)
+
+print(ma.a)
+
+# 使用import方式进行导入,只能跟包名和模块名
+# 使用from...import可以导入包,模块,函数,变量
